4915fa5468390c43f3a223c19dc43d3238a26c88
Lakshya Kanungo <lakshyaislearning@gmail.com>
2025-07-29 10:17:11 +0530

    Setted up chrome extension settings

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
+  base: './',
+  rollupOptions: {
+      input: {
+        popup: resolve("dist", 'index.html'), // Your main HTML file
+        // If you have a background script:
+        // background: resolve(__dirname, 'src/background.js'),
+        // If you have a content script:
+        // 'content-script': resolve(__dirname, 'src/content-script.js'),
+      },
+      output: {
+        // Keep original filenames for easier debugging
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name]-[hash].js',
+        assetFileNames: '[name]-[hash].[ext]',
+      },
+    },
 })
